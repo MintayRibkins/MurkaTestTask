30030651681e55d3089cc1347e4bbc7d4d7fa218
samua544 <dmitrysamsolution@gmail.com>
2017-03-26 18:24:50 +0300

    add    - удалить информацию о матче         - получить список матчей         - получить список матчей, сыгранных заданным игроком         - получить список матчей, начавшихся в определенном интервале дат-времен         - получить информацию об одном матче

--- src/AppBundle/Controller/MatchController.php
+++ src/AppBundle/Controller/MatchController.php
@@ -2,24 +2,40 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use AppBundle\Entity\Match;
-use FOS\RestBundle\Controller\Annotations\Route;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class MatchController
+class MatchController extends Controller
 {
-
     /**
      * @ApiDoc()
-     * @Route("matches/{id}", methods={"GET"})
+     * @Rest\Route("matches/{id}", methods={"GET"})
      * @ParamConverter("match", class="AppBundle:Match")
      * @Rest\View()
      */
     public function viewAction(Match $match)
     {
         return $match;
+    }
+
+    /**
+     * @ApiDoc()
+     * @Rest\Route("matches/{id}", methods={"DELETE"})
+     * @ParamConverter("match", class="AppBundle:Match")
+     * @Rest\View()
+     */
+    public function deleteAction(Match $match)
+    {
+        $em = $this->getDoctrine()->getManager();
+        /* @var $em EntityManagerInterface */
+
+        $em->remove($match);
+        $em->flush();
+
+        return array('success' => true);
     }
 }
--- src/AppBundle/Controller/MatchesController.php
+++ src/AppBundle/Controller/MatchesController.php
@@ -4,28 +4,60 @@ namespace AppBundle\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Controller\Annotations\Route;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class MatchesController extends Controller
 {
 
     /**
      * @ApiDoc()
-     * @Route("/matches")
-     * @Method("GET")
+     * @Rest\Route("/matches")
+     * @Rest\QueryParam(name="page", requirements="\d+", default="1")
+     * @Rest\QueryParam(name="player", requirements="\d+")
+     * @Rest\QueryParam(name="date_from", requirements="\d\d\.\d\d\.\d\d\d\d", default=null, allowBlank=true)
+     * @Rest\QueryParam(name="date_to", requirements="\d\d\.\d\d\.\d\d\d\d", default=null, allowBlank=true)
+     * @ParamConverter("date_from", class="DateTime", converter="datetime", options={"format": "d.m.Y"})
+     * @ParamConverter("date_to", class="DateTime", converter="datetime", options={"format": "d.m.Y"})
      * @Rest\View()
      */
-    public function listAction()
+    public function listAction(Request $request, $page = 1, $player = null)
     {
         $em = $this->getDoctrine()->getManager();
         /* @var $em EntityManagerInterface */
 
-        $qb = $em->getRepository('AppBundle:Match')->createQueryBuilder('m')->setMaxResults(10);
+        $date_from = $request->attributes->get('date_from');
+        $date_to = $request->attributes->get('date_to');
 
-        return array('matches' => $qb->getQuery()->getResult());
+        $qb = $em->getRepository('AppBundle:Match')
+            ->createQueryBuilder('m');
+
+        if ($player) {
+            $qb
+                ->join('m.matchPlayers', 'mp', 'WITH', 'mp.player = :player')
+                ->setParameter('player', $player);
+        }
+
+        if ($date_from && $date_to) {
+            $qb
+                ->andWhere('m.startTime BETWEEN :date_from AND :date_to')
+                ->setParameter('date_from', $date_from)
+                ->setParameter('date_to', $date_to);
+        }
+
+        $pagerfanta = new Pagerfanta(new DoctrineORMAdapter($qb, false));
+        $pagerfanta->setMaxPerPage(20);
+        $pagerfanta->setCurrentPage($page);
+
+        return array(
+            'matches' => iterator_to_array($pagerfanta->getCurrentPageResults()),
+            'count' => $pagerfanta->getNbResults(),
+            'pages' => $pagerfanta->getNbPages(),
+        );
 
     }
 }
