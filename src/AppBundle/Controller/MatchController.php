0c9c3bea5a6c41d808fe4f5061083073ab3cb78b
samua544 <dmitrysamsolution@gmail.com>
2017-03-26 19:20:14 +0300

    adding Matches

--- src/AppBundle/Controller/MatchController.php
+++ src/AppBundle/Controller/MatchController.php
@@ -2,14 +2,16 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\MatchType;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use AppBundle\Entity\Match;
+use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-class MatchController extends Controller
+class MatchController extends FOSRestController
 {
     /**
      * @ApiDoc()
@@ -18,6 +20,50 @@ class MatchController extends Controller
      * @Rest\View()
      */
     public function viewAction(Match $match)
+    {
+        return $match;
+    }
+
+    /**
+     * @ApiDoc(
+     *   resource = true,
+     *   input = "AppBundle\Form\MatchType",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     400 = "Returned when the form has errors"
+     *   }
+     * )
+     * @Rest\Route("matches", methods={"POST"})
+     * @Rest\View()
+     */
+    public function newAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        /* @var $em EntityManagerInterface */
+
+        $match = new Match();
+        $form = $this->createForm(MatchType::class, $match);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em->persist($match);
+            $em->flush();
+
+            return $match;
+        }
+
+        return array(
+            'form' => $form
+        );
+    }
+
+    /**
+     * @ApiDoc()
+     * @Rest\Route("matches/{id}", methods={"POST"})
+     * @ParamConverter("match", class="AppBundle:Match")
+     * @Rest\View()
+     */
+    public function editAction(Match $match)
     {
         return $match;
     }
